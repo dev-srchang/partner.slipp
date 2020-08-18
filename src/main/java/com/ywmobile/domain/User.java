d0821b36587117c49e26297f8cde3ccfa85d2e01
ywm.srchang <srchang@ywmobile.co.kr>
2020-08-18 19:06:48 +0900

    2020.08.18 - 16강, 17강  - H2 database 추가  - JPA 추가  - QnA form 추가 하지 않음 (16강 내용)

--- src/main/java/com/ywmobile/domain/User.java
+++ src/main/java/com/ywmobile/domain/User.java
@@ -1,9 +1,25 @@
 package com.ywmobile.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(nullable = false, length = 20)
 	String partnerId;
+
+	@Column(nullable = false)
 	String pwd;
+
+	@Column(nullable = false)
 	String partnerName;
+
 	String email;
 
 	public void setPartnerId(String partnerId) {
---
+++ src/main/java/com/ywmobile/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ywmobile.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ywmobile.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
