82b8ee336bbfec988f63278e6a4eae438e46181f
dev-srchang <dev.srchang@gmail.com>
2020-08-20 18:01:13 +0900

    2020.08.20  - 24강 적용 완료   -- login 기능 추가   -- JPA repositoy 커스텀 메소드 추가   -- HttpSession 정보 사용하여 로그인 구성

--- src/main/java/com/ywmobile/controller/UserController.java
+++ src/main/java/com/ywmobile/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.ywmobile.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,38 +18,65 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
-	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	@RequestMapping(value = "/create", method = RequestMethod.POST) // Action : registerForm 에서 "등록" 버튼을 눌렀을 시
 	public String create(User user) {
 		System.out.println("user : " + user.toString());
 		userRepository.save(user);
-		return "redirect:/user/list"; // call list method
+		return "redirect:/user/listTable"; // Result : redirect to listTable.html
 	}
 
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public String list(Model model) {
+	@RequestMapping(value = "/listTable", method = RequestMethod.GET) // Action : navbar에서 "사람 모양" 아이콘 메뉴를 눌렀을 시
+	public String listTable(Model model) {
 		model.addAttribute("users", userRepository.findAll());
-		return "/user/list";
+		return "/user/listTable"; // Result : move to listTable.html
 	}
 
-	@RequestMapping(value = "/form", method = RequestMethod.GET)
-	public String form() {
-		return "/user/form";
+	@RequestMapping(value = "/registerForm", method = RequestMethod.GET) // Action : navbar에서 "파트너사 등록" 메뉴 버튼을 눌렀을 시
+	public String registerForm() {
+		return "/user/registerForm"; // Result : move to registerForm.html
 	}
 
-	@RequestMapping(value = "/{id}/form", method = RequestMethod.GET)
+	@RequestMapping(value = "/{id}/updateForm", method = RequestMethod.GET) // Action : listTable 에서 "수정" 버튼을 눌렀을 시
 	public String updateForm(@PathVariable Long id, Model model) {
 		User user = userRepository.findById(id).orElse(null);
 //		System.out.println("User : " + user.toString());
 		model.addAttribute("user", user);
-		return "/user/updateForm";
+		return "/user/updateForm"; // Result : move to updateForm.html
 	}
 
-	@RequestMapping(value = "/{id}", method = {RequestMethod.POST, RequestMethod.PUT})
+	@RequestMapping(value = "/{id}", method = {RequestMethod.POST, RequestMethod.PUT}) // Action : updateForm 에서 "수정 완료" 버튼을 눌렀을 시
 	public String update(@PathVariable Long id, User newUser) {
 		User user = userRepository.findById(id).orElse(null);
 		user.update(newUser);
 //		System.out.println("new user : " + user.toString());
 		userRepository.save(user);
-		return "redirect:/user/list"; // call list method
+		return "redirect:/user/listTable"; // Result : move to listTable.html
+	}
+
+	@RequestMapping(value = "/loginForm", method = RequestMethod.GET) // Action : navbar에서 "로그인" 메뉴 버튼 을 눌렀을 시
+	public String loginForm() {
+		return "/user/loginForm"; // Result : move to loginForm.html
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST) // Action : loginForm 에서 "로그인" 버튼을 눌렀을 시
+	public String login(String partnerId, String pwd, HttpSession httpSession) {
+		User user = userRepository.findByPartnerId(partnerId);
+
+		if (userRepository.findByPartnerId(partnerId) == null) {
+			System.out.println("Login failure 1");
+			return "redirect:/user/loginForm";
+		}
+
+		if (!pwd.equals(user.getPwd())) {
+			System.out.println("Login failure 2");
+			return "redirect:/user/loginForm";
+		}
+
+		System.out.println("Login success");
+
+		// 세션에 저장 로직
+		httpSession.setAttribute("user", user);
+
+		return "redirect:/"; // Result : move to index.html
 	}
 }
--- src/main/java/com/ywmobile/repository/UserRepository.java
+++ src/main/java/com/ywmobile/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ywmobile.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	User findByPartnerId(String partnerId);
 }
