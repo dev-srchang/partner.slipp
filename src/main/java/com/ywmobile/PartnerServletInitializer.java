ae3ba84d6899d6e7f587d6101a4d7b340135dca6
dev-srchang <dev.srchang@gmail.com>
2020-08-23 16:22:01 +0900

    2020.08.23  - 29강 적용 완료   -- 외부 tomcat 사용 + war 빌드

--- src/main/java/com/ywmobile/PartnerServletInitializer.java
+++ src/main/java/com/ywmobile/PartnerServletInitializer.java
@@ -3,11 +3,9 @@ package com.ywmobile;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
-
+public class PartnerServletInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
+		return application.sources(PartnerApplication.class);
 	}
-
 }
